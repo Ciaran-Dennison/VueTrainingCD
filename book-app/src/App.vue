f0a274d92c3ca1bdf43b88a3e8c95c14787f5ace
Ciaran Dennison <ciaran.dennison@audacia.co.uk>
2026-05-27 13:38:31 +0100

    chore/fix: set up component based infrastructure and fixed CSS issues

--- book-app/src/App.vue
+++ book-app/src/App.vue
@@ -1,101 +1,12 @@
 <script setup lang="ts">
-import { ref } from 'vue'
-
-const form = ref({
-  firstname: '',
-  lastname: '',
-  birthdate: '',
-  eye_colour: '',
-  email: '',
-  username: '',
-  address1: '',
-  address2: '',
-  address3: '',
-  city: '',
-  county: '',
-  postcode: '',
-  phone: ''
-})
-
-const handleSubmit = () => {
-  let fname = form.value.firstname;
-  let sname = form.value.lastname;
-  let bday = form.value.birthdate;
-  let colour = form.value.eye_colour;
-  let email = form.value.email;
-  let username = form.value.username;
-  let add1 = form.value.address1;
-  let add2 = form.value.address2;
-  let add3 = form.value.address3;
-  let city = form.value.city;
-  let county = form.value.county;
-  let postcode = form.value.postcode;
-  let phone = form.value.phone;
-  ("Form Submitted" + fname + sname + bday + colour + email + username + add1 + add2 + add3 + city + county + postcode + phone);
-  alert(fname + sname + bday + colour + email + username + add1 + add2 + add3 + city + county + postcode + phone)
-}
-
+import UserForm from "./components/UserForm.vue";
 </script>
 
 
 
 <template>
-  <html>
-      <head>
-          <link rel="stylesheet" type="text/css" href="./UserForm.css">
-          <title>User Details Form</title>
-      </head>
-      <body>
-          <h1>User Details Form</h1>
-          <!-- <action="/submit" method="post"-->
-          <form id = "userform" @submit.prevent="handleSubmit">
-              <fieldset>
-                  <div>
-                      <label for="firstname">First Name</label> </br> <input class="grey-background" type="text" id="firstname" v-model="form.firstname"/>
-                      </br>
-                      <label for="lastname">Last Name</label> </br> <input class="grey-background" type="text" id="lastname" v-model="form.lastname"/>
-                      </br>
-                      <label for="birthdate">Date of Birth</label> </br> <input class="grey-background" type="date" id="birthdate" v-model="form.birthdate"/>
-                      </br>
-                      <label for="colour">Eye Colour</label> <br> <select class="grey-background" id="colour" v-model="form.eye_colour">
-                          <option value="Blue">Blue</option>
-                          <option value="Green">Green</option>
-                          <option value="Brown">Brown</option>
-                          <option value="Hazel">Hazel</option>
-                          <option value="Grey">Grey</option>
-                          <option value="Red">Red</option>
-                      </select>
-                  </div>
-                  <div>
-                      <label for="email">Email</label> </br> <input class="grey-background" type="email" id="email" v-model="form.email"/>
-                      </br>
-                      <label for="username">Username</label> </br> <input class="grey-background" type="text" id="username" v-model="username"/>
-                      
-                  </div>
-                  <div>
-                      <label for="address1">Address Line 1</label> </br> <input class="grey-background" type="text" id="address1" v-model="address1"/>
-                      </br>
-                      <label for="address2">Address Line 2</label> </br> <input class="grey-background" type="text" id="address2" v-model="address2"/>
-                      </br>
-                      <label for="address3">Address Line 3</label> </br> <input class="grey-background" type="text" id="address3" v-model="address3"/>
-                  </div>
-                  <div>
-                      <label for="city">City/Town</label> </br> <input class="grey-background" type="text" id="city" v-model="form.city"/>  
-                      </br>
-                      <label for="county">County</label> </br> <input class="grey-background" type="text" id="county" v-model="form.county"/> 
-                      </br>
-                      <label for="postcode">Postcode</label> </br> <input class="grey-background" type="text" id="postcode" v-model="form.postcode"/>
-                  </div>
-                  <div>
-                      <label for="phone">Phone Number</label> </br> <input class="grey-background" type="number" id="phone" v-model="form.phone"/>
-                  </div>
-                  <div>
-                      <button type="submit">Submit</button>
-                  </div>
-              </fieldset>
-          </form>
-      </body>
-  </html>
+<UserForm/>
 </template>
 
-<style scoped></style>
+<style scoped>
+</style>
