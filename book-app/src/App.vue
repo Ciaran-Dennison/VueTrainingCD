348544f4636e7549f96bfa1b36b53b61218915d4
Ciaran Dennison <ciaran.dennison@audacia.co.uk>
2026-05-28 10:47:34 +0100

    feat: added homepage #4

--- book-app/src/App.vue
+++ book-app/src/App.vue
@@ -1,11 +1,14 @@
 <script setup lang="ts">
 import UserForm from "./components/UserForm.vue";
 import EmployeeTable from "./components/EmployeeTable.vue"
+import Homepage from "./components/Homepage.vue";
+import image from "./images/audacia_academy_logo.jpg"
 </script>
 
 
 
 <template>
+<Homepage/>
 <UserForm/>
 <EmployeeTable/>
 </template>
---
+++ book-app/src/components/Homepage.vue
@@ -0,0 +1,61 @@
+<script setup lang="ts">
+
+</script>
+
+
+
+<template>
+<html>
+    <head>
+        <title>Homepage</title>
+    </head>
+    <body>
+        <div>
+        <img src="@/images/audacia_academy_logo.jpg" alt="Academy Logo" width="450" height="300">
+        </div>
+        <div>
+        <button alert="Add view here for User Form">User Form</button>
+        </div>
+        <br>
+        <br>
+        <br>
+        <div>
+        <button alert="Add view here for Employee Table">Employee Table</button>
+        </div>
+    </body>
+</html>
+</template>
+
+<style scoped>
+h1 {
+    margin-top: 0;
+    text-align: center;
+}
+
+img {
+    display:block;
+    margin: auto;
+}
+
+body {
+    background-color: #064179;
+}
+
+button {
+    display: block;
+    margin: auto;
+    background-color: white;
+    border-color: grey;
+    border-radius: 10px;
+    height: 100px;
+    width: 100px;
+}
+
+button:hover {
+    background-color: lightgrey;
+}
+
+button:focus {
+    background-color: grey;
+}
+</style>
