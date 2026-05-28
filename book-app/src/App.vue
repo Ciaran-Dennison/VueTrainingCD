433e3bfeddfc060318991a22427a987a4aee3efc
Ciaran Dennison <ciaran.dennison@audacia.co.uk>
2026-05-28 12:10:33 +0100

    feat: beautified navbar #4

--- book-app/src/App.vue
+++ book-app/src/App.vue
@@ -6,9 +6,9 @@
 
 <template>
     <nav>
-        <router-link to="/">Home</router-link> |
-        <router-link to="/UserForm">User Form</router-link> |
-        <router-link to="/EmployeeTable">Employee Table</router-link>       
+        <button><router-link to="/">Home</router-link></button> |
+        <button><router-link to="/UserForm">User Form</router-link></button> |
+        <button><router-link to="/EmployeeTable">Employee Table</router-link></button>       
     </nav>
 
     <router-view/>
@@ -18,11 +18,22 @@
 
 nav{
     background-color: #74a2cf;
-    text-align: center;
 }
 
 html {
     background-color: #064179;
     min-height: 100vh;
+}
+
+button {
+    display: inline-flexbox;
+    align-items: center;
+    justify-content: center;
+    background-color: white;
+    border-color: grey;
+    border-radius: 10px;
+    height: 30px;
+    width: fit-content;
+    margin: auto;
 }
 </style>
